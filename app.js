881a887a329c152b0214545ea2be32ce2bbf677c
annoinspace <aneesahalmask@gmail.com>
2022-11-10 15:12:18 +0000

    details page logic

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-// fetch("https://striveschool-api.herokuapp.com/api/product/", {
-//   headers: {
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjY1OWQ0YmUzZDAwMTU4NDYwMGUiLCJpYXQiOjE2NjgwODUzMzgsImV4cCI6MTY2OTI5NDkzOH0.XJBbNPVHhiXi6sNOp7RKJVQqrx5QvsUISghLvKqJGMI"
-//   }
-// })
+// ------------------ HOMEPAGE ------------------//
+// ------------------ HOMEPAGE ------------------//
+// ------------------ HOMEPAGE ------------------//
 
 const options = {
   method: "GET",
@@ -33,7 +30,8 @@ function renderProducts(listOfProducts) {
     <div class="card-body">
       <h4 class="card-text text-center">${product.name}</h4>
       <p class="card-text text-center">${product.description}</p>
-    </div>
+      <a href="details.html?productId=${product._id}">VIEW DETAILS</a>
+    </div>     
   </div>`
 
     cards.appendChild(productCard)
@@ -43,3 +41,34 @@ window.onload = async () => {
   const products = await getProducts()
   renderProducts(products)
 }
+
+// ------------------ DETAILS PAGE ------------------//
+// ------------------ DETAILS PAGE ------------------//
+// ------------------ DETAILS PAGE ------------------//
+
+// const params = new URLSearchParams(window.location.search)
+
+// const productId = params.get("productId")
+
+// async function getProduct() {
+//   const response = await fetch(
+//     `https://striveschool-api.herokuapp.com/api/product/${product._id}`
+//   )
+//   const product = await response.json()
+//   return product
+// }
+
+// function renderProduct(product) {
+//   document.getElementById("img-detail").innerHTML = ` <div>
+// <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+// </div>`
+
+//   document.getElementById("text-detail").innerHTML = `<div>
+// <h4 class="text-center">${product.name}</h4>
+// </div>`
+// }
+
+// window.onload = async () => {
+//   const product = await getProduct()
+//   renderProduct(product)
+// }
